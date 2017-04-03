f584344abaf4b9d8aedae9e2eaed23f8073e76f2
zhuozhi12 <feibou@126.com>
2017-04-03 16:34:51 +0800

    create edit update destroy

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,7 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit]
+  before_action :find_post_and_check_permission , :only => [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
   end
+
+  def edit
+
+
+end
 
 
   def create
@@ -21,7 +27,33 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+
+
+    if @post.update(post_params)
+
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @post.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to account_posts_path
+  end
+
   private
+
+  def find_post_and_check_permission
+    @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission"
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
